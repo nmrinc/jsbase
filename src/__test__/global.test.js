e23defd895e330f42df2cb8573f474d100cbb557
Erick Avila <nmr.inc@icloud.com>
2021-03-22 13:22:53 -0600

    Lesson 4. Boolean / Array Tests

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -3,14 +3,55 @@
 //@o It's important to add the .test suffix to tell the compiler that this file it's about tests and not included into the build.
 
 const text = 'Hello World!';
+const fruits = ['apple', 'melon', 'banana'];
+
 /**
  * @a Declare the test, that will receive 2 props.
  * @o 1. The expected description
  * @o 2. An anonymous function that will deploy the test.
 */
+// @concept String comparison test
 test('Must have matching text', () => {
   //@a Declare the expect variable that defines what to expect and the comparison
   expect(text).toMatch(/World/);
+});
+
+
+// @concept Array comparison test
+test('Must contain a banana', () => {
+  expect(fruits).toContain('banana');
+});
+
+
+// @concept Numbers comparison test
+test('Greater than 10', () => {
+  expect(10).toBeGreaterThan(9);
+});
+
+
+// @concept Boolean comparison test
+test(`It's true?`, () => {
+  expect(true).toBeTruthy();
+});
+
+
+// @concept Boolean comparison test
+test(`It's False?`, () => {
+  expect(false).toBeFalsy();
+});
+
+
+// @concept Callback comparison test
+
+// @o This callback receives an string, split it and reverse the content.
+const reverseString = (str, callback) => {
+  callback(str.split('').reverse().join(''));
+}
+
+test(`Testing a callback`, () => {
+  reverseString('Hello', (str) => {
+    expect(str).toBe('olleH');
+  })
 });
 
 /**
